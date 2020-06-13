b1350ed3caba53348474f838e4a7d45ef85d7a75
Sergey <bashkin2013@mail.ru>
2020-06-13 22:39:17 +0400

    feat: added styles for header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -6,22 +6,26 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { pagePropType } from '../router/pages';
+import Styles from './styles';
 
 const Header = ({ pages }) => {
     const location = useLocation();
     return (
-        <AppBar position="fixed">
+        <AppBar position="fixed" style={Styles.header}>
             <Toolbar>
                 <Typography variant="h6">RS Lang</Typography>
             </Toolbar>
             <nav>
-                <ul>
+                <ul style={Styles.li}>
                     {pages.map((e) => {
                         const current = e.url === location.pathname;
                         return (
                             <li key={`li_${e.url}`}>
                                 <Button>
-                                    <Link style={{ color: current ? 'black' : 'blue' }} to={e.url}>
+                                    <Link
+                                        style={{ color: current ? '#cfcfcf' : 'white' }}
+                                        to={e.url}
+                                    >
                                         {e.title}
                                     </Link>
                                 </Button>
---
+++ src/components/header/styles.jsx
@@ -0,0 +1,11 @@
+export default {
+    header: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    li: {
+        display: 'flex',
+        listStyle: 'none',
+    },
+};
