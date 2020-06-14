52af5f9e2cbc732383f76c51d590c538707ef257
Sergey <bashkin2013@mail.ru>
2020-06-14 16:39:14 +0400

    refactor: styles of header replaced to scss styles

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -6,17 +6,16 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { pagePropType } from '../router/pages';
-import Styles from './styles';
 
 const Header = ({ pages }) => {
     const location = useLocation();
     return (
-        <AppBar position="fixed" style={Styles.header}>
+        <AppBar position="fixed" className="header">
             <Toolbar>
                 <Typography variant="h6">RS Lang</Typography>
             </Toolbar>
             <nav>
-                <ul style={Styles.li}>
+                <ul className="header__links">
                     {pages.map((e) => {
                         const current = e.url === location.pathname;
                         return (
