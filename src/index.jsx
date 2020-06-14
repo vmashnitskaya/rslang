f3497f18dfdd0788eadc1b7741939845b083548a
Sergey <bashkin2013@mail.ru>
2020-06-14 21:48:02 +0400

    refactor: removed global styles

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './style/styles.scss';
 
 ReactDOM.render(
     <React.StrictMode>
