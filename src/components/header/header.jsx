3b555aad47e8b8d26afb68105af5ee567778c2ac
Sergey <bashkin2013@mail.ru>
2020-06-19 20:06:15 +0400

    feat: added sidebar component on header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,23 +4,51 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuIcon from '@material-ui/icons/Menu';
+import PropTypes from 'prop-types';
+import Sidebar from '../sidebar';
+import { pagePropType } from '../router/pages';
 import './header.scss';
 
-const Header = () => (
-    <AppBar position="fixed" className="header">
-        <Toolbar>
-            <Typography variant="h6">RS Lang</Typography>
-        </Toolbar>
-        <nav className="header__nav">
-            <ul className="header__links">
-                <li>
-                    <IconButton aria-label="account of current user" color="inherit">
-                        <AccountCircle />
+const Header = ({ pages }) => {
+    // const [open, setOpen] = React.useState(false);
+    // const [setOpen] = React.useState(false);
+    // const handleDrawerOpen = () => {
+    //     setOpen(true);
+    // };
+    // const handleDrawerClose = () => {
+    //     setOpen(false);
+    // };
+    return (
+        <div>
+            <AppBar position="fixed" className="header">
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        // onClick={handleDrawerOpen}
+                    >
+                        <MenuIcon />
                     </IconButton>
-                </li>
-            </ul>
-        </nav>
-    </AppBar>
-);
+                    <Typography variant="h6">RS Lang</Typography>
+                </Toolbar>
+                <nav className="header__nav">
+                    <ul className="header__links">
+                        <li>
+                            <IconButton aria-label="account of current user" color="inherit">
+                                <AccountCircle />
+                            </IconButton>
+                        </li>
+                    </ul>
+                </nav>
+            </AppBar>
+            <Sidebar pages={pages} />
+        </div>
+    );
+};
+
+Header.propTypes = {
+    pages: PropTypes.arrayOf(pagePropType).isRequired,
+};
 
 export default Header;
