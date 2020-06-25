8bdc9afb98214f43d2e545bc22882ce1a0de7f78
Volha <volha.mashnitskaya@gmail.com>
2020-06-25 15:15:19 +0300

    fix: fix integration issues

--- src/components/router/index.jsx
+++ src/components/router/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { connect, useSelector } from 'react-redux';
 import PageWrapper from './pageWrapper';
 import Header from '../header/index';
