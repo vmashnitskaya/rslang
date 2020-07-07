4dac97f23b42cfa00dbba1ac676e90c6aed82a5e
KarinaLogvina <karinalogvina0@gmail.com>
2020-07-07 21:55:51 +0300

    feat: modal witth game rules

--- src/components/sprint/Sprint.jsx
+++ src/components/sprint/Sprint.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
 import StartGame from './StartGame';
 
 const Sprint = () => {
+    const [started, setStart] = useState(false);
+    const onStart = useCallback(() => setStart(true));
+
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/sprint">
-                    <StartGame />
-                    <App className="sprint-game" />
+                    <StartGame onClose={onStart} />
+                    {started && <App className="sprint-game" />}
                 </Route>
             </Switch>
         </BrowserRouter>
--- src/components/sprint/StartGame.jsx
+++ src/components/sprint/StartGame.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -10,8 +11,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import CloseIcon from '@material-ui/icons/Close';
-import Slide from '@material-ui/core/Slide';
 
 const useStyles = makeStyles((theme) => ({
     appBar: {
@@ -23,21 +22,18 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const Transition = React.forwardRef(function Transition(props, ref) {
-    return <Slide direction="up" ref={ref} />;
-});
-
-export default function StartGame() {
+export default function StartGame({ onClose }) {
     const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = useState(true);
 
     const handleClose = () => {
+        onClose();
         setOpen(false);
     };
 
     return (
         <div>
-            <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+            <Dialog fullScreen open={open} onClose={handleClose}>
                 <AppBar className={classes.appBar}>
                     <Toolbar>
                         <IconButton
@@ -45,27 +41,27 @@ export default function StartGame() {
                             color="inherit"
                             onClick={handleClose}
                             aria-label="close"
-                        >
-                            <CloseIcon />
-                        </IconButton>
+                        />
                         <Typography variant="h6" className={classes.title}>
                             Rules
                         </Typography>
-                        <Button autoFocus color="inherit" onClick={handleClose}>
-                            Start Game
-                        </Button>
                     </Toolbar>
                 </AppBar>
-                <List>
-                    <ListItem button>
-                        <ListItemText primary="Phone ringtone" secondary="Titania" />
-                    </ListItem>
-                    <Divider />
-                    <ListItem button>
-                        <ListItemText primary="Default notification ringtone" secondary="Tethys" />
-                    </ListItem>
-                </List>
+                <Box>
+                    <ComplexityPoints
+                        currentComplexity={complexity}
+                        onComplexityChange={handleComplexityChange}
+                        complexityArray={[0, 1, 2, 3, 4, 5]}
+                    />
+                </Box>
+                <Button variant="contained" color="primary" onClick={handleClose}>
+                    Start Game
+                </Button>
             </Dialog>
         </div>
     );
 }
+
+StartGame.propTypes = {
+    onClose: PropTypes.func.isRequired,
+};
