f5b359dedf4f30170fe5d2c2979e8580355fbbcc
Sergey Bashkin <bashkin2013@mail.ru>
2020-07-10 11:48:25 +0400

    fix: algorithm not right worked

--- src/components/audition/number.js
+++ src/components/audition/number.js
@@ -6,8 +6,8 @@ export const createArrayOfUniqueNumbers = (number, min, max) => {
     const array = [];
     for (let i = 0; i < number; i += 1) {
         let randomNumber = generateRandomNumber(min, max);
-        while (array.includes(number)) {
-            randomNumber = generateRandomNumber();
+        while (array.includes(randomNumber)) {
+            randomNumber = generateRandomNumber(min, max);
         }
         array.push(randomNumber);
     }
