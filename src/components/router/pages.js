999be04dcc01403110ebac7ae70c56f90469cf5a
Toffifi <sanyq@mail.ru>
2020-07-15 22:01:15 +0300

    fix: another merge bugs

--- src/components/router/pages.js
+++ src/components/router/pages.js
@@ -17,6 +17,7 @@ import LevelTest from '../LevelTest/LevelTest';
 const pages = [
     {
         component: MainPage,
+        title: 'Main Page',
         url: '/',
         exact: true,
         img: '',
@@ -28,6 +29,7 @@ const pages = [
     },
     {
         component: MainGame,
+        title: 'Words learning',
         url: '/learn',
         exact: false,
         img: '/assets/images/maingame-card.jpg',
@@ -39,6 +41,7 @@ const pages = [
     },
     {
         component: Vocabulary,
+        title: 'Vocabulary',
         url: '/vocabulary',
         exact: false,
         img: '/assets/images/vocabulary.png',
@@ -49,18 +52,8 @@ const pages = [
         description: '',
     },
     {
-        component: Sprint,
-        url: '/sprint',
-        img: '/assets/images/sprint-card.jpg',
-        exact: false,
-        userMenuPage: false,
-        mainPage: true,
-        auth: false,
-        minigame: true,
-        description: 'Determine if the word and translation match in 1 minute.',
-    },
-    {
         component: SpeakItGame,
+        title: 'SpeakIt',
         url: '/speakit',
         img: '/assets/images/speakit-card.jpg',
         exact: true,
@@ -72,6 +65,7 @@ const pages = [
     },
     {
         component: EnglishPuzzle,
+        title: 'English-Puzzle',
         url: '/englishpuzzle',
         exact: true,
         userMenuPage: false,
@@ -83,6 +77,7 @@ const pages = [
     },
     {
         component: Audition,
+        title: 'Audition',
         url: '/audition',
         img: '/assets/images/audition-card.jpg',
         exact: true,
@@ -94,6 +89,7 @@ const pages = [
     },
     {
         component: SavannahGame,
+        title: 'Savannah',
         url: '/Savannah',
         img: '/assets/images/savannah-card.jpg',
         exact: true,
@@ -105,6 +101,7 @@ const pages = [
     },
     {
         component: SettingsPage,
+        title: 'Settings',
         url: '/settings',
         img: '/assets/images/settings.png',
         exact: true,
@@ -116,6 +113,7 @@ const pages = [
     },
     {
         component: AnagrammGame,
+        title: 'Anagramm Game',
         url: '/anagramm',
         exact: true,
         img: '/assets/images/anagramm-card.jpg',
@@ -126,7 +124,20 @@ const pages = [
         description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
     },
     {
+        component: Sprint,
+        title: 'Sprint',
+        url: '/sprint',
+        img: '/assets/images/sprint-card.jpg',
+        exact: false,
+        userMenuPage: false,
+        mainPage: true,
+        auth: false,
+        minigame: true,
+        description: 'Determine if the word and translation match in 1 minute.',
+    },
+    {
         component: AboutProject,
+        title: 'About Project',
         url: '/aboutproject',
         exact: true,
         img: '/assets/images/angry.jpg',
@@ -138,6 +149,7 @@ const pages = [
     },
     {
         component: AboutTeam,
+        title: 'About Team',
         url: '/aboutteam',
         img: '/assets/images/happy.jpg',
         exact: true,
@@ -175,6 +187,7 @@ const pages = [
 
 export const pagePropType = PropTypes.exact({
     component: PropTypes.elementType.isRequired,
+    title: PropTypes.string.isRequired,
     url: PropTypes.string.isRequired,
     exact: PropTypes.bool.isRequired,
     img: PropTypes.string.isRequired,
